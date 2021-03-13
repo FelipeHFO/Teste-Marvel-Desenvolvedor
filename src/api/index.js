994157918f245b462bf50115a5ac78b6fa5f1456
Felipe Oliveira <felipe.henrique.f.o@gmail.com>
2021-03-13 18:00:07 -0300

    feat: show personagens details

--- src/api/index.js
+++ src/api/index.js
@@ -18,6 +18,27 @@ app.get('/characters/:characterId', (req, res) => {
   res.send(personagem);
 })
 
+app.get('/characters/:characterId/comics', (req, res) => {
+  const { comics } = data.characters.find(char => char.id === parseInt(req.params.characterId));
+  res.send(comics);
+})
+
+app.get('/characters/:characterId/events', (req, res) => {
+  const { events } = data.characters.find(char => char.id === parseInt(req.params.characterId));
+  res.send(events);
+})
+
+app.get('/characters/:characterId/series', (req, res) => {
+  const { series } = data.characters.find(char => char.id === parseInt(req.params.characterId));
+  const { available, items } = series;
+  res.send({ available, items });
+})
+
+app.get('/characters/:characterId/stories', (req, res) => {
+  const { stories } = data.characters.find(char => char.id === parseInt(req.params.characterId));
+  res.send(stories);
+})
+
 app.listen(PORT, () => {
   console.log('Servidor online!');
 })
--- src/components/Personagem/Personagem.js
+++ src/components/Personagem/Personagem.js
@@ -1,8 +1,94 @@
-function Personagem({name, description}) {
+import React, { useState } from 'react';
+import api from '../../api/api';
+
+function Personagem({id, name, description}) {
+  const [visible, setVisible] = useState({
+    comics: false, 
+    events: false, 
+    series: false, 
+    stories: false
+  });
+
+  const [comics, setComics] = useState('');
+  const [events, setEvents] = useState('');
+  const [series, setSeries] = useState('');
+  const [stories, setStories] = useState('');
+  
+  async function handle(params) {
+    const { data } = await api.get(`characters/${id}/${params}`);
+    
+    if(params === 'comics'){
+      setComics(data);
+      setVisible({comics: true, events: false, series: false, stories: false});
+    }
+    if(params === 'events'){
+      setEvents(data);
+      setVisible({comics: false, events: true, series: false, stories: false});
+    }
+    if(params === 'series'){
+      setSeries(data);
+      setVisible({comics: false, events: false, series: true, stories: false});
+    }
+    if(params === 'stories'){
+      setStories(data);
+      setVisible({comics: false, events: false, series: false, stories: true});
+    }
+  }
+
+  const RenderComics = () => {
+    const isVisible = comics !== '' && visible.comics === true;
+
+    if(!isVisible)
+      return null;
+
+    return comics.items.map(item => <li key={id+item.name}>{item.name}</li>)
+  }
+
+  const RenderEvents = () => {
+    const isVisible = events !== '' && visible.events === true;
+
+    if(!isVisible)
+      return null;
+
+    return events.items.map(item => <li key={id+item.name}>{item.name}</li>)
+  }
+
+  const RenderSeries = () => {
+    const isVisible = series !== '' && visible.series === true;
+
+    if(!isVisible)
+      return null;
+
+    return series.items.map(item => <li key={id+item.name}>{item.name}</li>)
+  }
+
+  const RenderStories = () => {
+    const isVisible = stories !== '' && visible.stories === true;
+
+    if(!isVisible)
+      return null;
+
+    return stories.items.map(item => <li key={id+item.name}>{item.name}</li>)
+  }
+
   return (
     <div>
       <h1>{name}</h1>
       <h3>{description}</h3>
+      <menu>
+        <button onClick={() => handle('comics') }>Comics</button>
+        <button onClick={() => handle('events')}>Events</button>
+        <button onClick={() => handle('series')}>Series</button>
+        <button onClick={() => handle('stories')}>Stories</button>
+      </menu>
+      <main>
+        <ul>
+          <RenderComics />
+          <RenderEvents />
+          <RenderSeries />
+          <RenderStories />
+        </ul>
+      </main>
     </div>
   )
 }
