5f44f1576784fb969c3f6712b660db470faa5433
Felipe Oliveira <felipe.henrique.f.o@gmail.com>
2021-03-13 18:06:44 -0300

    refactor: delete named routes of comics, events, series and stories / feat: add response status to API answers

--- src/routes/routeNames.js
+++ src/routes/routeNames.js
@@ -1,6 +1,2 @@
 export const PERSONAGENS_PAGE_PATH = '/v1/public/characters/'
 export const PERSONAGEM_PAGE_PATH = '/v1/public/characters/:id'
-export const COMICS_PAGE_PATH = '/v1/public/characters/:id/comics'
-export const EVENTS_PAGE_PATH = '/v1/public/characters/:id/events'
-export const SERIES_PAGE_PATH = '/v1/public/characters/:id/series'
-export const STORIES_PAGE_PATH = '/v1/public/characters/:id/stories'
