5d0de5d47e8cc7e383fbe3f65f0878a6a6fbfe77
Felipe Oliveira <felipe.henrique.f.o@gmail.com>
2021-03-13 13:14:28 -0300

    feat: add CORS to project to access api on frontend

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const data = require('../data/data.json');
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/characters', (req, res) => {
