cf09f4656239e5d8f5466b82f48a98bc50060f32
Felipe Oliveira <felipe.henrique.f.o@gmail.com>
2021-03-13 22:08:28 -0300

    feat: create page not found

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const data = require('../data/data.json');
+const data = require('./data/data.json');
 
 app.use(cors());
 app.use(express.json());
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DefaultLayout from '../layouts/DefaultLayout';
+
+function NotFoundPage() {
+  return (
+    <DefaultLayout>
+      <h1>Not found Page</h1>
+    </DefaultLayout>
+  )
+}
+
+export default NotFoundPage;
